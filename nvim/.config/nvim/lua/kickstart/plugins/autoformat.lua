c297434afbb5b83c74a848f330a278c07a99ef86
Sebastien Cunin <sebastien.cunin.ext@orange.com>
2024-04-29 09:16:01 +0200

    disable the autoformat for the php files.

--- nvim/.config/nvim/lua/kickstart/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/autoformat.lua
@@ -50,6 +50,11 @@ return {
           return
         end
 
+
+        -- juste pour les projets Drupal,,car on se trouve dans le PSR2 et pas celui de Drupal et DrupalPratice.
+        if client.name == 'intelephense' then
+          return
+        end
         -- Create an autocmd that will run *before* we save the buffer.
         --  Run the formatting command for the LSP that has just attached.
         vim.api.nvim_create_autocmd('BufWritePre', {
