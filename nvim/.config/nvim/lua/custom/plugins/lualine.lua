8f5fdd89874331d528dcd345dce3f34d80d8679b
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2024-05-08 07:33:10 +0200

    New file plugin lualine

---
+++ nvim/.config/nvim/lua/custom/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return
+{
+	-- Set lualine as statusline
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	-- See `:help lualine.txt`
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = 'auto',
+			component_separators = '|',
+			section_separators = '',
+		},
+	},
+}
