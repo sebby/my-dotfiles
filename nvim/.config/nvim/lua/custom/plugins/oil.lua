a5c8710ec9dbd2e68b6677ae9ef748fba1dd2ca0
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2024-05-17 08:53:08 +0200

    OIL afterplugin.

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -1,11 +1,19 @@
 return {
+	{
+		'stevearc/oil.nvim',
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
 
-	'stevearc/oil.nvim',
-	opts = {},
-	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup()
-		-- TODO revoir les keymaps sur ce plugin
-	end
+
+
+
+
+
+
+			})
+		end
+	}
 }
