3002597d6f875200c3aa1da45d7d1ea629a7f7a3
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2024-06-04 07:01:16 +0200

    djlint

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -602,6 +602,7 @@ local servers = {
   -- rust_analyzer = {},
   -- tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
+  -- djlint = { filetypes = { 'html', 'twig', 'hbs' } },
 
   lua_ls = {
     Lua = {
--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -5,21 +5,21 @@
 return {
 	-- { "ellisonleao/gruvbox.nvim", priority = 1000,   config = true },
 	{ "tjdevries/colorbuddy.nvim" },
-	{
-		"rebelot/kanagawa.nvim",
-		priority = 1000,
-		init = function()
-			--  Color Scheme kanagawa.
-			-- wave the default heart-warming theme,
-			-- dragon for those late-night sessions
-			-- lotus for when you're out in the open.
-			vim.cmd.colorscheme 'kanagawa-lotus'
-		end,
-		config = function()
-			print 'hello world'
-		end
-
-	},
+	-- {
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	priority = 1000,
+	-- 	init = function()
+	-- 		--  Color Scheme kanagawa.
+	-- 		-- wave the default heart-warming theme,
+	-- 		-- dragon for those late-night sessions
+	-- 		-- lotus for when you're out in the open.
+	-- 		vim.cmd.colorscheme 'kanagawa-lotus'
+	-- 	end,
+	-- 	config = function()
+	-- 		print 'hello world'
+	-- 	end
+	--
+	-- },
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 	}
