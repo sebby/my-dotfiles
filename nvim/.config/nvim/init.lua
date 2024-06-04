c84e2f94a840bd8b49312f3c3c39cb9230ee89f4
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2024-06-04 07:17:42 +0200

    Add the lspkind inside , draft, before my own PDE.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -104,6 +104,7 @@ require('lazy').setup({
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
+    lazy = false,
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
       {
@@ -123,7 +124,8 @@ require('lazy').setup({
       -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
-
+      'hrsh7th/cmp-buffer',
+      "onsails/lspkind.nvim",
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
     },
@@ -647,6 +649,7 @@ mason_lspconfig.setup_handlers {
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
 local cmp = require 'cmp'
+local lspkind = require("lspkind")
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
@@ -658,7 +661,7 @@ cmp.setup {
     end,
   },
   completion = {
-    completeopt = 'menu,menuone,noinsert',
+    completeopt = 'menu,menuone,noselect',
   },
   mapping = cmp.mapping.preset.insert {
     ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -693,7 +696,27 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'path' },
+    { name = 'buffer' },
   },
+  formatting = {
+    format = lspkind.cmp_format({
+
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      -- can also be a function to dynamically calculate max width such as
+      -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(entry, vim_item)
+        return vim_item
+      end
+
+
+    })
+  }
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
