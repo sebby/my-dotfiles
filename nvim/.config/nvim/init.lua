57494f16a2e6205d34e43b386590843c7e92cee1
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2024-06-04 07:23:30 +0200

    Config lspkind.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -666,8 +666,6 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
@@ -699,6 +697,8 @@ cmp.setup {
     { name = 'buffer' },
   },
   formatting = {
+    fields = { "kind", "abbr", "menu" },
+    expandable_indicator = true,
     format = lspkind.cmp_format({
 
       mode = 'symbol', -- show only symbol annotations
@@ -707,12 +707,6 @@ cmp.setup {
       -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
       ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
       show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function(entry, vim_item)
-        return vim_item
-      end
 
 
     })
