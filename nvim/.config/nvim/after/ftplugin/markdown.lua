322882719e9fc49ff62440cb005049c017705ed2
Sebastien Cunin <640614+sebby@users.noreply.github.com>
2024-06-06 21:28:31 +0200

    Ajout d'un fichier ftplugin pour les fichiers de markdown.

---
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+-- TODO modifier le colorscheme
+-- TODO activer le spell check pour le français
+print(" je suis dans un fichier markdown")
+vim.cmd.colorscheme "zellner"
+vim.wo.spell = true
+vim.o.spelllang = "fr"
