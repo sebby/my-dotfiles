1286e02c755a1d29dd24d58464541d1ae87f52cf
Sebastien Cunin <sebastien.cunin.ext@orange.com>
2024-07-25 09:37:29 +0200

    downgrade kitty

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -132,7 +132,11 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
+  {
+    'folke/which-key.nvim',
+    tag = 'v2.0.0',
+    opts = {}
+  },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -222,7 +226,7 @@ require('lazy').setup({
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  { 'numToStr/Comment.nvim',  opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
   {
