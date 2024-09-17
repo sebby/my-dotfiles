9e17e570c4becca48cf3a5159f2c1d2ab482a94a
Sebastien Cunin <sebastien.cunin.ext@orange.com>
2024-09-17 09:43:28 +0200

    Some files for the geany tool.

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -59,7 +59,6 @@ return {
 	}
 	,
 	{
-
 		"romainl/Apprentice",
 		-- enabled = false,
 
@@ -70,19 +69,20 @@ return {
 
 	}
 
-	, {
-	"shaunsingh/nord.nvim",
-	enabled = true,
-	config = function()
-		vim.cmd.colorscheme "nord"
-		vim.g.nord_contrast = true
-		vim.g.nord_borders = false
-		vim.g.nord_disable_background = false
-		vim.g.nord_italic = true
-		vim.g.nord_uniform_diff_background = true
-		vim.g.nord_bold = false
-	end
+	,
+	{
+		"shaunsingh/nord.nvim",
+		-- enabled = true,
+		config = function()
+			vim.cmd.colorscheme "nord"
+			vim.g.nord_contrast = true
+			vim.g.nord_borders = false
+			vim.g.nord_disable_background = false
+			vim.g.nord_italic = true
+			vim.g.nord_uniform_diff_background = true
+			vim.g.nord_bold = false
+		end
 
-}
+	}
 
 }
